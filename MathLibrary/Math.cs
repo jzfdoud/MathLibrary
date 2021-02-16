e073338daa43dc5659f63918f4355d8c4c8f0489
Jada <jada.fagan@gmail.com>
2021-02-16 14:47:43 -0500

    Add project files.

---
+++ MathLibrary/Math.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary
+{
+    class Math
+    {
+        public static int Addition(int nbr1, int nbr2)
+        {
+            return nbr1 + nbr2;
+        }
+
+        public static int Subtraction(int nbr1, int nbr2)
+        {
+            return nbr1 - nbr2;
+        }
+
+        public static int Multiplication(int nbr1, int nbr2)
+        {
+            return nbr1 * nbr2;
+        }
+
+        public static int Division(int nbr1, int nbr2)
+        {
+            return nbr1 / nbr2;
+        }
+
+        public static int Modulo(int nbr1, int nbr2)
+        {
+            return nbr1 - (nbr1 / nbr2 * nbr2);
+        }
+
+        public static int Power(int nbr1, int nbr2)
+        {
+            var answer = 1;
+            for( var idx = 0; idx < nbr2; idx++)
+            {
+                answer *= nbr1;
+            }
+            return answer;
+        }
+
+        public static bool isEven(int i)
+        {
+            if((Modulo(i,2)) == 0)
+            {
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public static int Inc(int i)
+        {
+            return i + 1;
+        }
+
+        public static int Dec(int i)
+        {
+            return i - 1;
+        }
+    }
+}
---
+++ MathLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //practice with creating static methods
+
+            //var math = new Math();
+            var answer = Math.Addition(1, 2);
+            Console.WriteLine($" 1 + 2 = {answer}");
+
+            answer = Math.Subtraction(7, 2);
+            Console.WriteLine($" 7 - 2 = {answer}");
+
+            answer = Math.Multiplication(3, 2);
+            Console.WriteLine($" 3 X 2 = {answer}");
+
+            answer = Math.Division(10, 5);
+            Console.WriteLine($" 10 / 5 = {answer}");
+
+            answer = Math.Modulo(7, 2); // should return 1
+            Console.WriteLine($" 7 % 2 = {answer}");
+
+            answer = Math.Modulo(13, 5); // should return 3
+            Console.WriteLine($" 13 % 5 = {answer}");
+
+            answer = Math.Modulo(1234, 13); // should return 12
+            Console.WriteLine($" 1234 % 13 = {answer}");
+
+            answer = Math.Power(13, 3); //2197
+            Console.WriteLine($" 13 ^ 3 = {answer}");
+
+            var i = 98;
+            var boolAnswer = Math.isEven(i);
+            Console.WriteLine($" {i} is {(boolAnswer ? "Even" : "Odd")}");
+
+            i = 0;
+            var inc = Math.Inc(i);
+            var dec = Math.Dec(i);
+            Console.WriteLine($" {i} incremented {Math.Inc(i)}");
+            Console.WriteLine($" {i} decremented {Math.Dec(i)}");
+
+        }
+    }
+}
